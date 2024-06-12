2ed98c6007e4669d5a5015bdaff3d6a1e4567d02
Chandra <71860963+rchandra29@users.noreply.github.com>
2024-06-12 20:58:04 +0700

    Update Assignment 1 & 2 with one Loop

--- assignment/src/assignment3.java
+++ assignment/src/assignment3.java
@@ -1,34 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class assignment3 {
+
+    public static int largest(int[] arr) {
+        int[] arrayCopy = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arrayCopy);
+        return arrayCopy[arrayCopy.length - 2];
+    }
 
     public static List<Integer> secondLargest(int[] arr) {
         List<Integer> index = new ArrayList<Integer>();
 
-        int biggestArr = arr[0];
-        int secBiggest = arr[0];
+        int secBiggest = largest(arr);
 
-        // Find the biggest element
         for (int i = 0; i < arr.length; i++) {
-            if (biggestArr < arr[i]) {
-                biggestArr = arr[i];
+            if (arr[i] == secBiggest) {
+                index.add(i);
             }
         }
+        // int biggestArr = arr[0];
+        // int secBiggest = arr[0];
 
-        // Find the second biggest element
-        for (int j = 0; j < arr.length; j++) {
-            if (secBiggest < arr[j] && arr[j] < biggestArr) {
-                secBiggest = arr[j];
-            }
-        }
+        // // Find the biggest element
+        // for (int i = 0; i < arr.length; i++) {
+        // if (biggestArr < arr[i]) {
+        // biggestArr = arr[i];
+        // }
+        // }
 
-        // Find indices of the second biggest element
-        for (int k = 0; k < arr.length; k++) {
-            if (arr[k] == secBiggest) {
-                index.add(k);
-            }
-        }
+        // // Find the second biggest element
+        // for (int j = 0; j < arr.length; j++) {
+        // if (secBiggest < arr[j] && arr[j] < biggestArr) {
+        // secBiggest = arr[j];
+        // }
+        // }
+
+        // // Find indices of the second biggest element
+        // for (int k = 0; k < arr.length; k++) {
+        // if (arr[k] == secBiggest) {
+        // index.add(k);
+        // }
+        // }
 
         return index;
     }
@@ -38,5 +52,6 @@ public class assignment3 {
 
         List<Integer> result = secondLargest(arr);
         System.out.println(result);
+
     }
 }
--- assignment/src/assignment4.java
+++ assignment/src/assignment4.java
@@ -1,26 +1,44 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class assignment4 {
 
     public static int[] subArray(int arr[], int n) {
-        int[] indexes = new int[2];
+
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            int sum = arr[i];
+            sum += arr[i];
+
             if (sum == 0) {
-                indexes[0] = i;
-                indexes[1] = i;
-                return indexes;
+                return new int[] { 0, i };
             }
-            for (int j = i + 1; j < n; j++) {
-                sum += arr[j];
-                if (sum == 0) {
-                    indexes[0] = i;
-                    indexes[1] = j;
-                    return indexes;
-                }
+
+            if (sumMap.containsKey(sum)) {
+                return new int[] { sumMap.get(sum) + 1, i };
             }
+
+            sumMap.put(sum, i);
         }
+        // int[] indexes = new int[2];
+
+        // for (int i = 0; i < n; i++) {
+        // int sum = arr[i];
+        // if (sum == 0) {
+        // indexes[0] = i;
+        // indexes[1] = i;
+        // return indexes;
+        // }
+        // for (int j = i + 1; j < n; j++) {
+        // sum += arr[j];
+        // if (sum == 0) {
+        // indexes[0] = i;
+        // indexes[1] = j;
+        // return indexes;
+        // }
+        // }
+        // }
         return new int[] { -1, -1 };
     }
 
