3738c1f2eadd8c10c8513220a9bd7413539e3bc6
Chandra <ryan.chandra.hadi@gmail.com>
2024-07-03 13:43:26 +0700

    week4

--- Week_03/Lecture_5/Assignment 1/src/main/java/com/dummyproject/dummyproject/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dummyproject.dummyproject.repository;
-
-import com.dummyproject.dummyproject.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact, String> {
-}
