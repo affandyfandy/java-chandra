af4c67b0d917dff295ad2e9cd99d566457f83763
Chandra <ryan.chandra.hadi@gmail.com>
2024-08-02 04:09:49 +0700

    feat: Lecture 16 done with two projects seperated

---
+++ Week_08/Lecture_16/products/src/main/java/com/service/products/dto/ProductDTO.java
@@ -0,0 +1,12 @@
+package com.service.products.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private double price;
+}
